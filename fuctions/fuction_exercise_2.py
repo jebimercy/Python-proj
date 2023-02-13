795ff4e5cf0488e8dc447fc6dfdef0a6b12f62b1
eMobilis <jebichiimercy129@gmail.com>
2023-02-13 09:03:46 +0300

    first commit

---
+++ fuctions/fuction_exercise_2.py
@@ -0,0 +1,8 @@
+def calculate_area():
+    num1 = int(input("Enter the width"))
+    num2 = int(input("Enter the length"))
+    area = num1 * num2
+    print(f"The area is {area} square metres")
+
+
+calculate_area()
